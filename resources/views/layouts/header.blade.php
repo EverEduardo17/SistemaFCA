39462b4b2d5fb1a34d1f8070a0056f65ed31781b
Ronny <ronny12301@gmail.com>
2023-04-19 13:47:30 -0600

    logo blanco agregado

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,13 @@
 <header>
     <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-md navbar-light fixed-top bg-color">
-        <a href="{{ url('/home') }}">
-            <img class="logo" src="{{ asset('img/logo-blanco.png') }}">
-        </a>
-        <a class="navbar-brand text-white" href="{{ url('/home') }}" >
-            Universidad Veracruzana
-        </a>
+        <img class="logo" src="{{ asset('img/logo-blanco.png') }}">
+        <a class="navbar-brand text-white" href="{{ url('/home') }}" >Universidad Veracruzana</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
             <span class="navbar-text text-white">
