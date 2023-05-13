c33ac585b3d28e27017a9abaf32cc37b1940d417
Ronny <ronny12301@gmail.com>
2023-05-13 03:13:39 -0600

    footer restaurado y con el estilo arreglado

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer mt-auto text-white text-center">
+<footer class="footer text-white text-center">
     <div class="container">
         &copy; {{ getYear() }} Universidad Veracruzana. Todos los derechos reservados.
     </div>
