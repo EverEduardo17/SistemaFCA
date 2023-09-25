922fdde41bad209927219cb2043561b67d665fe7
Rousent <86386444+Rousent@users.noreply.github.com>
2023-09-25 11:46:37 -0600

    Separacion de vista de calendario y tabla
    Añadido pestañas para ver el calendario o la tabla. Modificación al css del filtro de estado.

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -24,49 +24,58 @@
         </div>
         <div class="col-lg-9 col-md-8">
         <!--<button class="btn btn-secondary mb-2" id="filterButton">Filtrar eventos</button>-->
-        
-        <form action="{{ route('eventos.index') }}" method="GET">
-            <div class="form-group">
-                <label for="lista-filtrar">Filtrar por estado:</label>
-                <select name="estado" id="lista-filtrar" class="form-control">
-                    <option value="">Todos</option>
-                    <option value="APROBADO">Aprobado</option>
-                    <option value="NO APROBADO">No aprobado</option>
-                    <option value="POR APROBAR">Pendiente</option>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary" style="display: inline-block; margin-left: 670px; margin-bottom: 10px;">Filtrar</button>
-        </form> 
 
-            <table id="table-jquery" class="display border-bottom" style="">
-                <thead>
-                    <tr>
-                        <th>Fecha</th>
-                        <th>Nombre</th>
-                        <th>Sede</th>
-                        <th>Estado</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($evento_fecha_sede_s as $efs)
-                    <tr>
-                        <td>{{$efs->fechaEvento->InicioFechaEvento ?? ""}}</td>
-                        <td>{{$efs->evento->NombreEvento ?? ""}}</td>
-                        <td>{{$efs->sedeEvento->NombreSedeEvento ?? ""}}</td>
-                        <td>{{$efs->evento->EstadoEvento ?? ""}}</td>
-                        <td>
-                            @can('havepermiso', 'eventos-leer')
-                                <a class="btn btn-sm btn-outline-primary" href="{{ route('eventos.show', [$efs->IdEvento]) }}"> Detalles</a>
-                            @endcan
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <form action="{{ route('eventos.index') }}" method="GET" style="display: flex">
+                <div class="form-group" style="display: flex; align-items: center; gap: 1rem">
+                    <label for="lista-filtrar" style="white-space: nowrap; margin: 0%">Filtrar por estado:</label>
+                    <select name="estado" id="lista-filtrar" class="form-control">
+                        <option value="">Todos</option>
+                        <option value="APROBADO">Aprobado</option>
+                        <option value="NO APROBADO">No aprobado</option>
+                        <option value="POR APROBAR">Pendiente</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary" style="display: inline-block; margin-left: 2rem; margin-bottom: 10px; height: fit-content">Filtrar</button>
+            </form>
+            <div class="nav nav-tabs" style="margin-bottom: 1rem">
+                <button id="tab-calendario" value="calendario" class="nav-link nav-link-focused">Calendario</button>
+                <button id="tab-tabla" value="tabla" class="nav-link">Tabla</button>
+            </div>
+            <div>
+                <div id="calendario">
+                    <div id="calendar"></div>
+                </div>
+                <div id="tabla" style="display: none">
+                    <table id="table-jquery" class="display border-bottom" style="">
+                        <thead>
+                            <tr>
+                                <th>Fecha</th>
+                                <th>Nombre</th>
+                                <th>Sede</th>
+                                <th>Estado</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($evento_fecha_sede_s as $efs)
+                            <tr>
+                                <td>{{$efs->fechaEvento->InicioFechaEvento ?? ""}}</td>
+                                <td>{{$efs->evento->NombreEvento ?? ""}}</td>
+                                <td>{{$efs->sedeEvento->NombreSedeEvento ?? ""}}</td>
+                                <td>{{$efs->evento->EstadoEvento ?? ""}}</td>
+                                <td>
+                                    @can('havepermiso', 'eventos-leer')
+                                        <a class="btn btn-sm btn-outline-primary" href="{{ route('eventos.show', [$efs->IdEvento]) }}"> Detalles</a>
+                                    @endcan
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-    <div id="calendar"></div>
 </div>
 {{-- {{ dd($calendar_events) }} --}}
 
@@ -82,6 +91,11 @@
         width: 220px;
         margin-left: auto;
         margin-right: auto;
+    }
+
+    .nav-link-focused {
+        background: #007bff;
+        color: aliceblue;
     }
 </style>
 @endsection
@@ -105,6 +119,26 @@
             "/" + e.date.getDate();
         //console.log(e.date.toISOString().slice(0,10) );
     });
+</script>
+
+<!-- Vistas -->
+<script defer>
+    const tabCalendario = document.getElementById("tab-calendario")
+    const tabTabla = document.getElementById("tab-tabla")
+    const calendario = document.getElementById("calendario")
+    const tabla = document.getElementById("tabla")
+
+    function changeTab(evt) {
+        tabCalendario.classList.remove("nav-link-focused")
+        tabTabla.classList.remove("nav-link-focused")
+        evt.target.classList.add("nav-link-focused")
+        calendario.style = "display: none"
+        tabla.style = "display: none"
+        document.getElementById(evt.target.value).style = "display: block"
+    }
+
+    tabCalendario.addEventListener("click", changeTab)
+    tabTabla.addEventListener("click", changeTab)
 </script>
 
 <!-- Full Calendar -->
