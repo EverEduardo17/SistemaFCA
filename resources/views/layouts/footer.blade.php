6b2ae3ab6dc3a744ea6ea262fa87ae04674dc6b5
Ronny <ronny12301@gmail.com>
2023-12-20 11:10:08 -0600

    CSS del footer arreglado para que siempre este encima

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,5 @@
-<footer class="footer text-white text-center">
+<footer class="footer text-white text-center" style=" z-index: 5;
+">
     <div class="container">
         &copy; {{ getYear() }} Universidad Veracruzana. Todos los derechos reservados.
     </div>
